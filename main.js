0cc57e395f49928b8365fe6efcbc10f200cd5ccb
ScorpJ <cansinlale01@gmail.com>
2025-12-06 01:06:56 +0300

    feat(pivot): move the pivot to test moving animations

--- main.js
+++ main.js
@@ -4,14 +4,34 @@ const context = canvas.getContext('2d');
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
-document.getElementById("hi-there").innerHTML = "as i said :)";
-
+let pivot = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    color: 'blue'
+};
 
 function drawPivotPoint() {
-    context.fillStyle = '#000';
+    context.fillStyle = pivot.color;
     context.beginPath();
-    context.arc(canvas.width / 2, canvas.height / 2, 10, 0, Math.PI * 2);
+    context.arc(pivot.x, pivot.y, 10, 0, Math.PI * 2);
     context.fill();
+    context.stroke();
 }
 
-drawPivotPoint()
+function updateGameArea() {
+    clear();
+    pivot.x += 1;
+    drawPivotPoint();
+    //console.log("running")
+}
+
+function clear() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function main() {
+    drawPivotPoint();
+    this.interval = setInterval(updateGameArea, 20);
+}
+
+main()
